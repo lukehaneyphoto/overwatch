b01dce7800bceb61d1de0da2517c7cdfe6e45422
lukehaneyphoto <lukehaneyphoto@gmail.com>
2020-02-24 23:43:52 -0500

    Update for debug

--- overwatch.js
+++ overwatch.js
@@ -1,1 +1,8 @@
-var 
+function showMenu() {
+    var menuElement = document.getElementById("myLinks");
+    if (menuElement.style.display === "block") {
+        menuElement.style.display = "none";
+    } else {
+        menuElement.style.display = "block";
+    }
+}
